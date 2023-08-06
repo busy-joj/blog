8a8aae27ed028f2cb5e413c9b42110bcf0c9d1fb
jyj <busy.joj@gmail.com>
2023-08-06 22:02:19 +0900

    Updates

---
+++ component---src-templates-blog-post-js-7e5fdc32963592e43491.js
@@ -0,0 +1,2 @@
+"use strict";(self.webpackChunkgatsby_starter_default=self.webpackChunkgatsby_starter_default||[]).push([[989],{4982:function(e,t,a){a.r(t);var l=a(7294);t.default=e=>{let{data:t}=e;const a=t.markdownRemark;return l.createElement("main",null,l.createElement("div",null,l.createElement("h1",null,a.frontmatter.title),l.createElement("div",{dangerouslySetInnerHTML:{__html:a.html}})))}}}]);
+//# sourceMappingURL=component---src-templates-blog-post-js-7e5fdc32963592e43491.js.map
